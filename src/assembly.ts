4687a5a589c2d2ae98618ff0f0425fda3c0fdceb
Gage Cottrell <inquisitorpinkie@gmail.com>
2023-07-16 22:14:24 -0700

    production time

--- src/assembly.ts
+++ src/assembly.ts
@@ -3,10 +3,11 @@ import { recipes, Items, assemblerSpeeds, timePerRecipe } from './values';
 import _ from 'lodash';
 import { SMap } from './smap';
 
+type productionTime = [ticksLeft: number, productCount: number];
 interface State {
     assemblers: {[p: string]: Partial<SMap<number>>};
     amountThatWeHave: { [p in Items]?: number };
-    inReserve: {[p in Items]?: {[k in Items]?: number}};
+    timeLeftInProduction: {[p in Items]?: productionTime};
     seen: Items[];
 }
 
@@ -14,26 +15,32 @@ const defaultState = {
     amountThatWeHave: {},
     assemblers: {},
     seen: [],
-    inReserve: {},
+    timeLeftInProduction: {},
 } satisfies State;
 
 const ex = localStorage.getItem('state');
 const existingStorage = {...defaultState, ...(ex ? JSON.parse(ex) : {})};
 
 
-function assemble(itemName: Items, assemblerCount: number, speed: number, timeStep: number, amounts: SMap<number>) {
+function assemble(itemName: Items, assemblerCount: number, speed: number, timeStep: number, amounts: SMap<number>, [ticksLeft, productCount]: productionTime): productionTime {
     const recipe = recipes[itemName as Items];
-    if (recipe === undefined) return;
+    if (recipe === undefined) return [0, 0];
 
     const amt = amounts[itemName as Items] ?? 0;
-
     const craftTime = timePerRecipe[itemName];
-    const TIME = timeStep * speed / craftTime;
+    const timePerTick = timeStep * speed / craftTime;
+
+    if (ticksLeft > 0) {
+        amounts[itemName] = amt + productCount * timePerTick;
+        return [ticksLeft - 1, productCount];
+    }
+
+    // not producing, so let's try to grab materials
 
     let numberOfRecipesToMake = assemblerCount ?? 0;
     if (numberOfRecipesToMake <= 0)
-        return;
-
+        return [0, 0];
+    
     _.toPairs(recipe).forEach(pair => {
         const [ingredientName, requiredCount] = pair;
         const weHave = amounts[ingredientName as Items] ?? 0;
@@ -41,28 +48,38 @@ function assemble(itemName: Items, assemblerCount: number, speed: number, timeSt
             numberOfRecipesToMake = 0;
         }
         else {
-            numberOfRecipesToMake = Math.min(weHave / requiredCount, numberOfRecipesToMake);
+            numberOfRecipesToMake = Math.min(Math.floor(weHave / requiredCount), numberOfRecipesToMake);
         }
     });
 
     _.toPairs(recipe).forEach(pair => {
         const [ingredientName, requiredCount] = pair;
+        const toGrab = numberOfRecipesToMake * requiredCount;
+
         const weHave = amounts[ingredientName as Items] ?? 0;
-        amounts[ingredientName as Items] = Math.max(0, weHave - (numberOfRecipesToMake * requiredCount * TIME));
+        amounts[ingredientName as Items] = Math.max(0, weHave - toGrab);
     });
 
-    amounts[itemName] = amt + numberOfRecipesToMake * TIME;
+    if (numberOfRecipesToMake <= 0)
+        return [0, 0];
+
+    const ticksToMake = Math.floor(1 / timePerTick);
+    return [ticksToMake, numberOfRecipesToMake];
 }
 
 
 function doProduction({
     assemblers,
     amountThatWeHave: _amountsThatWeHave,
+    timeLeftInProduction,
 }: State, timeStep: number) {
     const amounts: { [p: string]: number } = { ..._amountsThatWeHave };
 
     _.keys(assemblers).sort().forEach(level => {
-        _.forEach(assemblers[level], (assemblerCount, itemName) => assemble(itemName as Items, assemblerCount ?? 0, assemblerSpeeds[level as Items] ?? 0, timeStep, amounts));
+        _.forEach(assemblers[level], (assemblerCount, itemName) => {
+            const time = assemble(itemName as Items, assemblerCount ?? 0, assemblerSpeeds[level as Items] ?? 0, timeStep, amounts, timeLeftInProduction[itemName as Items] ?? [0, 0]);
+            timeLeftInProduction[itemName as Items] = time;
+        });
     });
 
     return {
