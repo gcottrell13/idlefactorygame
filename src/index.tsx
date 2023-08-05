1fe157a731f81d9134948bf31b8abca7f2f8025d
Gage Cottrell <inquisitorpinkie@gmail.com>
2023-08-05 11:01:34 -0700

    build on release publish

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const TICKS_PER_SECOND = 20;
 const root = createRoot(document.getElementById("view")!);
 root.render(<App ticksPerSecond={TICKS_PER_SECOND} />);
 
-document.title = "idlefactorygame v" + VERSION;
+document.title = "idlefactorygame v" + VERSION().join(".");
 document.addEventListener(
     "mousedown",
     function (event) {
